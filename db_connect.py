0e3844df7c5eeaafe6aa6730ab94e1ac19b7ad20
Oksyyy <abricosova.oxi@gmail.com>
2026-04-23 07:21:42 +0000

    Initial files were pushed empty in github - trying to fix the local environment and push the actual contentof the files

---
+++ db_connect.py
@@ -0,0 +1,24 @@
+import pymysql
+
+conn = None
+
+def connect():
+    global conn
+    conn = pymysql.connect(host="localhost",
+                            user="root",
+                            password="root",
+                            db="appdbproj",
+                            coursorclass=pymysql.coursors.DictCursor
+                            )
+    
+def view_speakers_sessions():
+    if (not conn):
+        connect();
+
+    query = "select * from table"
+
+    with conn:
+        coursor = conn.coursor()
+        coursor.execute(query)
+        x = coursor.fetchall()
+        print(x) 
